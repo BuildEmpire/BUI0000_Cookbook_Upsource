912e6ca52232148531a7655afa49d26acb0c00ba
Fred Thompson <fred.thompson@buildempire.co.uk>
2016-05-08 13:51:05 +0100

    Increased the version to 3.0.4291.

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,9 +2,9 @@ default['java']['install_flavor'] = 'oracle'
 default['java']['jdk_version'] = '8'
 default['java']['oracle']['accept_oracle_download_terms'] = true
 
-default['cookbook_upsource']['upsource']['version'] = '2.5.4995'
+default['cookbook_upsource']['upsource']['version'] = '3.0.4291'
 default['cookbook_upsource']['upsource']['install_root_dir'] = '/opt/upsource'
 default['cookbook_upsource']['upsource']['data_dir'] = '/root/.UpsourceData'
 default['cookbook_upsource']['upsource']['backup_dir'] = '/root/.UpsourceBackup'
-default['cookbook_upsource']['upsource']['download_url'] = 'https://download.jetbrains.com/upsource/upsource-2.5.4995.zip'
+default['cookbook_upsource']['upsource']['download_url'] = 'https://download.jetbrains.com/upsource/upsource-3.0.4291.zip'
 default['cookbook_upsource']['upsource']['memory_options'] = '-Xmx1g'
