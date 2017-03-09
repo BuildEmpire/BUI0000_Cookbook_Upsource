e818fbe5db0e5ac6f9e2f340cb9680d1aa96c949
Michael Wright <michael.wright@buildempire.co.uk>
2017-03-09 09:11:04 +0000

    Upgrade to 2017.1.1781

--- attributes/default.rb
+++ attributes/default.rb
@@ -2,10 +2,10 @@ default['java']['install_flavor'] = 'oracle'
 default['java']['jdk_version'] = '8'
 default['java']['oracle']['accept_oracle_download_terms'] = true
 
-default['cookbook_upsource']['upsource']['version'] = '3.5.3597'
+default['cookbook_upsource']['upsource']['version'] = '2017.1.1781'
 default['cookbook_upsource']['upsource']['install_root_dir'] = '/opt/upsource'
 default['cookbook_upsource']['upsource']['data_dir'] = '/root/.UpsourceData'
 default['cookbook_upsource']['upsource']['backup_dir'] = '/root/.UpsourceBackup'
-default['cookbook_upsource']['upsource']['download_url'] = 'https://download.jetbrains.com/upsource/upsource-3.5.3597.zip'
+default['cookbook_upsource']['upsource']['download_url'] = 'https://download.jetbrains.com/upsource/upsource-2017.1.1781.zip'
 default['cookbook_upsource']['upsource']['memory_options'] = '-Xmx1g'
-default['cookbook_upsource']['upsource']['shell_script_path'] = '/opt/upsource/3.5.3597/upsource-3.5.3597/bin/upsource.sh'
+default['cookbook_upsource']['upsource']['shell_script_path'] = '/opt/upsource/2017.1.1781/upsource-2017.1.1781/bin/upsource.sh'
